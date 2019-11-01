b0bdfab2735cbd3a5670e37e44bcd966fbb19fc4
vistait2018 <jidedorcas@gmail.com>
2019-11-01 13:50:50 +0100

    IncreamentAndDecreament

--- src/IncreamentAndDecrement.kt
+++ src/IncreamentAndDecrement.kt
@@ -1,8 +1,6 @@
-
 /**
  * Created by jyde on 11/1/2019.
  */
-
 /**
  * Oreations Rules
  * 1 ()
@@ -10,14 +8,12 @@
  * 3 * , /
  * 4 + ,-
  * 5 =
+ * 6 ++ ,--
  */
 
 fun main(args:Array<String>){
-    var n1:Int=10
-    var n2:Int =10
-    var n3:Int = 5
-    var sum:Int?
-    sum = n1+n2*n3-3
-
-    println("sun: $sum")
+    var x = 10;
+    var y = 11;
+    var z = ++x+y //(++x) = (x= x+1), (--x) = (x= x-1)
+    println(z);
 }
