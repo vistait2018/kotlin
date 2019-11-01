70b3c75a4fa35f0dd9b1a047a5f74d8cc06e6ba7
vistait2018 <jidedorcas@gmail.com>
2019-11-01 13:28:24 +0100

    ConvertDataTypes

---
+++ src/ConvertDataTypes.kt
@@ -0,0 +1,24 @@
+/**
+ * Created by jyde on 11/1/2019.
+ */
+
+
+fun main(args:Array<String>){
+    var n1:Int=10
+    var n2:Int?
+    var n2Str:String = "12"
+    n2= n2Str.toInt()
+    var n2Float:Float?
+     n2Float = n2Str.toFloat()
+
+    println("n1: "+ n1)
+    println("n2: "+ n2)
+    println("n2Strin: "+ n2Str)
+    println("n2Float: "+ n2Float)
+
+
+    var xpi:Double = 3.14
+    println("xpi: "+ xpi)
+    var IntPi:Int= xpi.toInt()
+    println("IntPi: "+ IntPi)
+}
